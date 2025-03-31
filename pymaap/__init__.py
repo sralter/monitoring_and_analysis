2ccfee4f886459bd1a8a0019c9c8dca9559292d1
sralter <s.r.alter@icloud.com>
2025-03-31 16:10:02 -0400

    update init and pyproject toml

--- pymaap/__init__.py
+++ pymaap/__init__.py
@@ -1,2 +1,9 @@
 from .monitoring import Timer, ErrorCatcher, get_metrics_start, get_metrics_end
 from .analysis import generate_plots, parse_log_lines, detect_recent_dense_block
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # for Python < 3.8
+
+__version__ = version("pymaap")
