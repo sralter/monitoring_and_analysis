8bf8f9f3b16c7ca0ecde4d6c0def1ca6fe500562
sralter <s.r.alter@icloud.com>
2025-04-28 13:46:52 -0400

    fix(logging_setup): change default console_level to INFO rather than DEBUG

--- pymaap/logging_setup.py
+++ pymaap/logging_setup.py
@@ -47,7 +47,7 @@ def init_general_logger(
     json_log: Optional[str] = "general.json.log",
     max_bytes: int = 10 * 1024 * 1024,
     backup_count: int = 5,
-    console_level: int = logging.DEBUG,
+    console_level: int = logging.INFO,
 ) -> logging.Logger:
     """
     Initialize the general-purpose logger for PyMAAP.
